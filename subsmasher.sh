3ef70391c13513af00623764835a2ccd783e4db6
alex-matty <alexmatty@protonmail.com>
2021-09-16 22:52:42 -0500

    Added google search functionality removed progress bar from cURL output
    To-do: Select which places to look for subdomains
    Add yahoo and bing as options for enumeration

--- subsmasher.sh
+++ subsmasher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script for subdomain ennumeration
+#Script for subdomain ennumeration
 
 #Check if cURL is installed, if not advise user and end script
 if ! command -v curl 2&>/dev/null
@@ -38,26 +38,37 @@ echo "-----------------------------------------------------------------------"
 echo -e "\n-----------------------------------------------------------------------"
 echo "[-] Domain Name: $domainName"
 echo "[-] Checking in:"
+echo "    [+] Google"
 echo "    [+] CRT.SH"
 echo "    [+] Threatcrowd"
 echo "    [+] sublist3r API"
+echo "[-] Output Filename: $outputFilename"
 echo -e "-----------------------------------------------------------------------\n"
 
 #Start subdomain enumeration
 
+#https://www.google.com SUBDOMAIN ENUMERATION
+echo "Checking Google"
+for pageStart in {0..500..10}
+do
+	curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" -s "https://www.google.com/search?q=$domainName&filter=0&biw=1366&bih=605&dpr=1&start=$pageStart" | grep -ioE '<a [^>]+>' | grep -ioE 'href="[^\"]+"' | grep -ioE '(http|https)://[^/"]+' | grep -ioE ".*$domainName" >> GoogleSubdomainsInBulk.txt
+done
+
+cat GoogleSubdomainsInBulk.txt | sort | uniq >> subdomainsInBulk.txt && rm GoogleSubdomainsInBulk.txt
+
 #https://crt.sh SUBDOMAIN ENUMERATION
 echo "Checking CRT.SH"
-curl "https://crt.sh/?q=$domainName" | grep -ioE "[^>]*\.*$domainName" | grep -ioE "[^=]*\.*$domainName" | grep -ioE "[^ ]*\.*$domainName" | sort | uniq >> subdomainBulk.txt
+curl -s "https://crt.sh/?q=$domainName" | grep -ioE "[^>]*\.*$domainName" | grep -ioE "[^=]*\.*$domainName" | grep -ioE "[^ ]*\.*$domainName" | sort | uniq >> subdomainsInBulk.txt
 
 #https://www.threatcrowd.org SUBDOMAIN ENUMERATION
 echo "Checking Threatcrowd"
-curl "https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=$domainName" | sed 's/,/\n/g' | grep -ioE "[^\"]*\.*$domainName" | grep -ioE "[^=]*\.*$domainName" | sort | uniq >> subdomainBulk.txt
+curl -s "https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=$domainName" | sed 's/,/\n/g' | grep -ioE "[^\"]*\.*$domainName" | grep -ioE "[^=]*\.*$domainName" | sort | uniq >> subdomainsInBulk.txt
 
 #https://api.sublist3r.com SUBDOMAIN ENUMERATION
 echo "Checking sublist3r api"
-curl "https://api.sublist3r.com/search.php?domain=$domainName" | sed 's/,/\n/g' | grep -ioE "[^\"]*\.*$domainName" | sort | uniq >> subdomainBulk.txt
+curl -s "https://api.sublist3r.com/search.php?domain=$domainName" | sed 's/,/\n/g' | grep -ioE "[^\"]*\.*$domainName" | sort | uniq >> subdomainsInBulk.txt
 
-cat subdomainBulk.txt | sort | uniq > $outputFilename && rm subdomainBulk.txt
+cat subdomainsInBulk.txt | sort | uniq > $outputFilename && rm subdomainsInBulk.txt
 
 foundSubdomains=$(wc -l $outputFilename | awk '{print$1}')
 
